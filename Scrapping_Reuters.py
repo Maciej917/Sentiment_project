dad215060f1661060051531ccf46a5b83b1f6504
unknown <maciej917pl@gmail.com>
2020-02-27 09:14:45 +0100

    Adding class for scrapping head,date,body from each article from csv

--- Scrapping_Reuters.py
+++ Scrapping_Reuters.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import time
-import json
+import csv
 
 
 class Scrapbot:
@@ -16,32 +14,36 @@ class Scrapbot:
     #######Funkcja openpage otwiera Reutersa na stronie danej firmy########
 
     def openpage(self):
-        url = "https://www.reuters.com/companies/{}.OQ".format(self.company)
-        bot = self.bot
-        bot.get(url)
+        self.bot.get("https://www.reuters.com/companies/{}.OQ".format(self.company))
         time.sleep(1)
-        cockie = bot.find_element_by_id("_evidon-banner-acceptbutton")
-        cockie.click()
+        self.bot.find_element_by_id("_evidon-banner-acceptbutton").click()
         time.sleep(1)
 
     ########Funkcja scrap_list_of_art ściąga ze strony linki do artykułów. Wywołująć funkcję możemy wybrać ilość razy jaką funkcja
 
     def scrap_list_of_art(self, times_of_scroll=0):
-        bot = self.bot
         art_heads_url_list = []
         for x in range(times_of_scroll):
-            bot.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+            self.bot.execute_script("window.scrollBy(0,document.body.scrollHeight)")
             time.sleep(2)
-        art_heads = bot.find_elements_by_class_name('MarketStoryItem-headline-2cgfz')
-        for art_head in art_heads:
-            art_head_url = art_head.get_attribute("href")
-            art_heads_url_list.append(art_head_url)
-        art_url_list_json = json.dumps(art_heads_url_list)
-        with open('art_list_json.json', 'w') as art_list_json:
-            json.dump(art_heads_url_list, art_list_json, indent=2)
+        for art_head in self.bot.find_elements_by_class_name('MarketStoryItem-headline-2cgfz'):
+            art_heads_url_list.append(art_head.get_attribute("href"))
+        print(art_heads_url_list)
+        with open('art_list.csv', 'w') as art_list_csv:
+            csv_writer = csv.writer(art_list_csv, delimiter = ',')
+            csv_writer.writerow(art_heads_url_list)
+
 
     #########Funkcja wyłącza webdriver#########
 
     def end(self):
-        bot = self.bot
-        bot.close()
+        self.bot.close()
+
+
+
+################# Przykładowe wywołanie #######################
+
+# scrap = Scrapbot("GOOGL")
+# scrap.openpage()
+# scrap.scrap_list_of_art(10)
+# scrap.end()
